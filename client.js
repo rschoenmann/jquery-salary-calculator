611d3cc7f3d3846a2a71193ea8485fa071a06adc
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-04-12 16:52:44 -0500

    buttonClick function works

--- client.js
+++ client.js
@@ -1,7 +1,18 @@
 console.log('js');
  $(document).ready(readyNow);
 
+let employeeArray = [];
+
  function readyNow(){
     console.log('jquery is ready');
+    buttonClick();
+ }//end readyNow
+
+ function buttonClick(){
+    $('#submitButton').on('click', addNewEmployee);
+ }//end buttonClick
+
+ function addNewEmployee(){
+    console.log('in addNewEmployee');
     
- }
+ }//end addNewEmployee
