c56cbb790000334ca86d13307f6a21b077f48360
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-04-12 17:22:55 -0500

    displayInputs function works now

--- client.js
+++ client.js
@@ -9,10 +9,31 @@ let employeeArray = [];
  }//end readyNow
 
  function buttonClick(){
+     //on button click, add new employee object
     $('#submitButton').on('click', addNewEmployee);
  }//end buttonClick
 
  function addNewEmployee(){
     console.log('in addNewEmployee');
-    
+    const newEmployeeObject = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        id: $('#idNumberIn').val(),
+        title: $('#titleIn').val(),
+        annualSalary: $('#annualSalaryIn').val()
+    }//end newEmployeeObject constant
+    employeeArray.push(newEmployeeObject);
+    displayInputs();
  }//end addNewEmployee
+
+ function displayInputs(){
+    let newest = employeeArray[employeeArray.length - 1];
+    $('#tableBody').append(`
+    <tr>
+    <td>${newest.firstName}</td>
+    <td>${newest.lastName}</td>
+    <td>${newest.id}</td>
+    <td>${newest.title}</td>
+    <td>${newest.annualSalary}</td>
+    </tr>`)
+ }//end displayInputs function
